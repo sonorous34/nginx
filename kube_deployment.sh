11991924fc42cbbcd74dcbc277c9802e803215d1
sonorous34 <sonorous34@naver.com>
2021-11-03 15:21:58 +0900

    nginx-cicd

--- kube_deployment.sh
+++ kube_deployment.sh
@@ -10,7 +10,7 @@ echo "NS_CHECK: ${NS_CHECK}"
 
 if [ -z ${NS_CHECK} ]||[ !${NS_CHECK} == 'cicd' ] ; then
   kubectl create ns ${K8S_NAMESPACE}
-  kubectl -n ${K8S_NAMESPACE} create secret generic ${K8S_SECRET} -from-literal=username=jenkins --from-literal=password='admin1234!'
+  kubectl -n ${K8S_NAMESPACE} create secret generic ${K8S_SECRET} --from-literal=username=jenkins --from-literal=password='admin1234!'
 fi
 
 kubectl -n ${K8S_NAMESPACE} apply -f ${K8S_DEPLOYMENT}
